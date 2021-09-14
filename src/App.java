f579942ff869551734503a3a12eee08508358eff
Maxim <maximgretsev@gmail.com>
2021-09-14 20:01:46 +0300

    Ответвление ветки

--- src/App.java
+++ src/App.java
@@ -3,6 +3,8 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello, Java!");
         System.out.println("Hello, World!");
+        System.out.println("Hello, LP!");
+        System.out.println("Hello, Git!");
 
     }
 }
